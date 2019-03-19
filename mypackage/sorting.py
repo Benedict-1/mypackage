d4c94c414feaa4feb9a30b42dcd7155a037bcd98
Benedict Rhodes <Benedict@explore-datascience.net>
2019-03-19 16:51:56 +0200

    new commits

--- mypackage/sorting.py
+++ mypackage/sorting.py
@@ -53,15 +53,16 @@ def partition(array, begin, end):
     array[pivot_idx], array[begin] = array[begin], array[pivot_idx]
     return pivot_idx
 
-def quick_sort_recursion(array, begin, end):
-    if begin >= end:
-        return
-    pivot_idx = partition(array, begin, end)
-    quick_sort_recursion(array, begin, pivot_idx-1)
-    quick_sort_recursion(array, pivot_idx+1, end)
+
 
 def quick_sort(array, begin=0, end=None):
     if end is None:
         end = len(array) - 1
+    def quick_sort_recursion(array, begin, end):
+        if begin >= end:
+            return
+        pivot_idx = partition(array, begin, end)
+        quick_sort_recursion(array, begin, pivot_idx-1)
+        quick_sort_recursion(array, pivot_idx+1, end)
 
     return quick_sort_recursion(array, begin, end)
