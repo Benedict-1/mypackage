9e106da378521fe1208014dc3317792a552d7a36
Benedict Rhodes <Benedict@explore-datascience.net>
2019-03-19 14:57:55 +0200

    New functions

--- mypackage/recursion.py
+++ mypackage/recursion.py
@@ -1,1 +1,35 @@
-def
+def sum_array(array):
+
+    '''Return sum of all items in array'''
+    import numpy as np
+    if len(array)==1:
+        return array[0]
+    else:
+        return array[0] + sum_array(np.delete(array,[0]))
+
+def fibonacci(n):
+
+    '''Return nth term in fibonacci sequence'''
+
+    if n <= 1:
+        return n
+
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+def factorial(n):
+
+    '''Return n!'''
+    if n==1:
+        return n
+    else:
+        return n*factorial(n-1)
+
+def reverse(word):
+
+    '''Return word in reverse'''
+    s=list(word)
+    if len(s)==1:
+        return s[0]
+    else:
+        return s.pop(-1)+reverse(s)
