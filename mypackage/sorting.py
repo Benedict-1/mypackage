7f2050ff70d33e6e3a602dc092743ca4d24dcd7c
Benedict Rhodes <Benedict@explore-datascience.net>
2019-03-19 17:08:12 +0200

    new

--- mypackage/sorting.py
+++ mypackage/sorting.py
@@ -44,23 +44,22 @@ def merge_sort(array):
     return merge(left, right)
 
 
-def partition(array, begin, end):
-    pivot_idx = begin
-    for i in range(begin+1, end+1):
-        if array[i] <= array[begin]:
-            pivot_idx += 1
-            array[i], array[pivot_idx] = array[pivot_idx], array[i]
-    array[pivot_idx], array[begin] = array[begin], array[pivot_idx]
-    return pivot_idx
+def partition(xs, start, end):
+    follower = leader = start
+    while leader < end:
+        if xs[leader] <= xs[end]:
+            xs[follower], xs[leader] = xs[leader], xs[follower]
+            follower += 1
+        leader += 1
+    xs[follower], xs[end] = xs[end], xs[follower]
+    return follower
 
+def _quicksort(xs, start, end):
+    if start >= end:
+        return
+    p = partition(xs, start, end)
+    _quicksort(xs, start, p-1)
+    _quicksort(xs, p+1, end)
 
-
-def quick_sort(array, begin=0, end=None):
-    def quick_sort_recursion(array, begin, end):
-        if begin >= end:
-            return
-        pivot_idx = partition(array, begin, end)
-        quick_sort_recursion(array, begin, pivot_idx-1)
-        quick_sort_recursion(array, pivot_idx+1, end)
-
-    return quick_sort_recursion(array, begin, end)
+def quick_sort(xs):
+    _quicksort(xs, 0, len(xs)-1)
