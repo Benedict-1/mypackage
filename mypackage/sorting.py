169ffb23ac4233473944ab14c4bc1c2d2d024290
Benedict Rhodes <benedict@explore>
2019-03-22 11:32:24 +0200

    new 3

--- mypackage/sorting.py
+++ mypackage/sorting.py
@@ -44,5 +44,22 @@ def merge_sort(array):
     return merge(left, right)
 
 
-def quick_sort(array):
-    return np.sort(array)
+def partition(xs, start, end):
+    follower = leader = start
+    while leader < end:
+        if xs[leader] <= xs[end]:
+            xs[follower], xs[leader] = xs[leader], xs[follower]
+            follower += 1
+        leader += 1
+    xs[follower], xs[end] = xs[end], xs[follower]
+    return follower
+
+def _quicksort(xs, start, end):
+    if start >= end:
+        return
+    p = partition(xs, start, end)
+    _quicksort(xs, start, p-1)
+    _quicksort(xs, p+1, end)
+
+def quick_sort(xs):
+    _quicksort(xs, 0, len(xs)-1)
